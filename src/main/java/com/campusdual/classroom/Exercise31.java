5ad92de0726a710de279213d280098f7b32edee5
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-09-27 13:13:22 +0200

    Ejercicio31 resuelto

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,26 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
 
+
+        File file  = new File("src/main/resources/lorem.txt");
+
+        try (FileReader fr = new FileReader(file)){
+            BufferedReader br = new BufferedReader(fr);
+            String linea=br.readLine();
+            while (linea!=null){
+                System.out.println(linea);
+                linea=br.readLine();
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
